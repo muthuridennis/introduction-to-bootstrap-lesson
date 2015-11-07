c7daa6fe1d00fdd1f25367cee48f90378fc1095a
dmuthuri <muthuridennis@gmail.com>
2015-11-07 12:40:20 +0300

    Add jquery validate and open source stuff section

--- README.md
+++ README.md
@@ -9,3 +9,32 @@
 ## Project roadmap
 - Add awesome pitch
 - Make works AJAX loaded 
+
+### Hosting this project using github pages
+- Change the repository name to <your_username>.github.io
+- To continue pushing to this project you'll need to update your remotes, so remove the old origin using `git remote remove origin` and add the new one.
+
+### Working with REST APIs
+- Install the following browser extensions:
+    - Chrome browser: Postman
+    - Firefox: Restclient
+
+### Data needed from github to display your repositories
+- name(repository name)
+- html_url(repository link)
+- teams_url(link to teams working on the repository)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js/main.js
+++ js/main.js
@@ -6,8 +6,10 @@ $(document).ready(function() {
 						$(this).removeClass('animated shake');
 					});
 	// Some people represent the event parameter as 'e'
-	// this is a naming shortcut 
-	$('#contact-form').on('submit', function(e){
+	// this is a naming shortcut
+	contactForm = $('#contact-form');
+
+	contactForm.on('submit', function(e){
 		// Place event handling logic here
 		url = '/';
 		data = $(this).serialize();
@@ -15,15 +17,74 @@ $(document).ready(function() {
 		$.post(url, data)
 		 .done(function() {
 				// if the data was submitted successfully: alert('You are logged in');
-				alert('You are logged in');		 	
+				console.log('You are logged in');		 	
 		 }).fail(function() {
 				// if the data was submitted and there was an error: alert('Ooops. Something went wrong');
-				alert('Ooops. Something went wrong'); 	
+				console.log('Ooops. Something went wrong'); 	
 		 });
 
 
 		// Prevents the default behaviour of the form
 		e.preventDefault();
 	});
+
+	contactForm.validate({
+		rules: {
+			fullname: "required",
+			email: {
+				required: true,
+				email: true
+			}
+		},
+		messages: {
+			fullname: "Please sepecify your fullname",
+			email: {
+				required: "We need your email address to reach you.",
+				email: "Please use the email format name@domain.com"
+			}
+		}
+	});
+
+	githubReposUrl = 'https://api.github.com/users/muthuridennis/repos';
+
+	$.get(githubReposUrl, function(repositories){
+		// parse repositories array
+		projectListTemplate = '';
+		projectListTemplate += '<ul>';
+
+		repositories.forEach(function(repository){
+			projectListTemplate += '<li>';
+				projectListTemplate += '<h2><a href="'+ repository.html_url +'">'+ repository.name +'</a></h2>';
+				projectListTemplate += '<p>'+ repository.description +'</p>';
+				projectListTemplate += '<button type="button" class="btn btn-default btn-lg">';
+	  			projectListTemplate += '<span class="glyphicon glyphicon-star" aria-hidden="true">'+ repository.stargazers_count +'</span>';
+  			projectListTemplate += '</button>'
+			projectListTemplate += '</li>';
+		});
+
+		projectListTemplate += '</ul>';
+
+		// projectListTemplate = '<ul>' <li>'
+		// 												+ '
+		// 						  					 	+	'<h2><a href="#">Introduction to bootstrap</a></h2>'
+		// 						  						+	'<p>My project description</p>'
+		// 						  						+ '<a href="#">Teams working on project</a>'
+		// 					  						+ '</li>'
+		// 				  						+ '</ul>';
+
+		$('#open-source-content').html(projectListTemplate);
+	})
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
